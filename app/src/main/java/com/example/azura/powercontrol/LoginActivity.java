53b1c6094af4f25602f8944261389ce9cd4ac10b
Akash Jain <jainaksh720@gmail.com>
2018-03-11 17:44:30 +0530

    First commit

---
+++ app/src/main/java/com/example/azura/powercontrol/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.azura.powercontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
